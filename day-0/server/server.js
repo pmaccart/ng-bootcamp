8180bf33bdf28dfbb56405fcc99051ac532c4b70
pmaccart <phil.maccart@slalom.com>
2014-03-11 21:20:02 -0500

    Day 0, Step 1: NodeJS Http Server

---
+++ day-0/server/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+ 
+http.createServer(
+  function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+  }
+).listen(8000);
+ 
+console.log('Server running at http://localhost:8000/');

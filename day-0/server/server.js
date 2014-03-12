6fd5bc55bbc80df7bfdd1837800b61fce7a1adfa
pmaccart <phil.maccart@slalom.com>
2014-03-11 22:08:42 -0500

    Day 0, Step 5: Express Server

--- day-0/server/server.js
+++ day-0/server/server.js
@@ -1,10 +1,22 @@
-var http = require('http');
- 
-http.createServer(
-  function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-  }
-).listen(8000);
- 
-console.log('Server running at http://localhost:8000/');
+var express = require('express');
+var app = express();
+
+app.use(express.bodyParser());
+
+app.get('/hello', function(req, res){
+  res.send('Hello World');
+});
+
+app.get('/hello.json', function(req, res) {
+  res.json({
+    message:"Hello"
+  });
+});
+
+app.use(function(req, res, next) {
+  res.send(404, 'Page not found!!!');
+})
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});

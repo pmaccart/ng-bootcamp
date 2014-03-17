7244ecf0231e9082d4140ad5108f254090286c5b
pmaccart <phil.maccart@slalom.com>
2014-03-16 23:00:33 -0500

    Day 1, Step 2: scope inheritance

--- day-1/app/app.spec.js
+++ day-1/app/app.spec.js
@@ -10,3 +10,27 @@ describe('MyController', function() {
     expect(scope.name).toBe('Phil');
   });
 });
+
+describe('ParentController', function() {
+  var controller, scope;
+  beforeEach(function() {
+    scope = {};
+    controller = new ParentController(scope);
+  });
+
+  it('should set the parent name property on the scope', function() {
+    expect(scope.parentName).toBe('Parent Controller');
+  })
+});
+
+describe('ChildController', function() {
+  var controller, scope;
+  beforeEach(function() {
+    scope = {};
+    controller = new ChildController(scope);
+  });
+
+  it('should set the child name property on the scope', function() {
+    expect(scope.childName).toBe('Child Controller');
+  })
+})

5181a376b5b481b8a77ab7094b20f0c0fc28e7b3
pmaccart <phil.maccart@slalom.com>
2014-03-24 23:26:48 -0500

    Day 3, Step 2: create a services module

--- day-3/client/app/app.js
+++ day-3/client/app/app.js
@@ -3,17 +3,31 @@ angular.module('myApp', ['myControllers'])
   console.log('Configuring the application');
 });
 
-angular.module('myControllers',[])
-.controller('MyController', function($scope, $http, $log) {
+angular.module('myControllers',['myServices'])
+.controller('MyController', function($scope, echonest) {
   $scope.getPlaylist = function(artist) {
-    $http.get('/api/playlist', {
-      params: {
-        artist: artist
-      }
-    }).then(function(response) {
-      $scope.playlist = response.data.response.songs;
+    echonest.getPlaylistByArtist(artist).then(function(playlist) {
+      $scope.playlist = playlist;
     }, function(err) {
       $log.error('Error loading tracks', err);
     });
   };
 });
+
+angular.module('myServices', [])
+.factory('echonest', function($http, $q) {
+
+  return {
+    getPlaylistByArtist: function(artist) {
+      var deferred = $q.defer();
+      $http.get('/api/playlist', {
+        params: { artist: artist }
+      }).then(function (response) {
+        deferred.resolve(response.data.response.songs);
+      }, function(error) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    }
+  };
+})

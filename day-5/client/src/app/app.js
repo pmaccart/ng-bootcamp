a3984f6cd327d08f1ef7cddde1d453128dd912bf
pmaccart <phil.maccart@slalom.com>
2014-04-07 23:51:35 -0500

    Day 5, Step 1: add routes

--- day-5/client/src/app/app.js
+++ day-5/client/src/app/app.js
@@ -5,6 +5,21 @@ angular.module('myApp', [
   'billing.controllers',
   'dashboard.controllers'
   ])
+.config(function ($routeProvider) {
+  $routeProvider.when('/dashboard', {
+    templateUrl: 'src/app/dashboard/dashboard.html',
+    controller: 'DashboardCtrl'
+  })
+  .when('/billing', {
+    templateUrl: 'src/app/billing/billing.html',
+    controller: 'BillingCtrl'
+  })
+  .when('/account', {
+    templateUrl: 'src/app/account/account.html',
+    controller: 'AccountCtrl'
+  })
+  .otherwise({redirectTo: '/dashboard'});
+})
 .controller('NavCtrl', function ($scope) {
   $scope.collapse1 = true;
   $scope.toggleCollapse = function() {
